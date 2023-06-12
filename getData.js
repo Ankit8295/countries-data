498b381a39d75765c0678089d2adee79d6ae0875
Ankit Kumar <112799319+Ankit8295@users.noreply.github.com>
2023-06-12 18:59:10 +0530

    fix

--- getData.js
+++ getData.js
@@ -1,15 +1,43 @@
 const countriesData = require("./countries.json");
+const countriesLength = require("./numberlength.json");
 
-function getData(fields) {
-  return countriesData.map((country) => {
-    data = { country: country.country };
-    fields.forEach((field) => {
-      data = { ...data, [field]: country[field] };
-    });
-    return data;
+const fs = require("fs");
+
+const final = [];
+
+countriesData.forEach((e) => {
+  countriesLength.forEach((c) => {
+    if (e.iso["alpha-2"] === c.code) {
+      final.push({
+        ...e,
+        phoneLength: c.phoneLength
+          ? typeof c.phoneLength == []
+            ? c.phoneLength
+            : [c.phoneLength]
+          : [],
+      });
+    }
+    return;
   });
-}
+});
 
-console.log(getData(["capital", "currency"]));
+fs.writeFile("last.json", JSON.stringify(final), (err) => {
+  if (err) console.log(err);
+  else {
+    console.log("File written successfully");
+  }
+});
 
-// console.log(countries);
+// function getData(fields) {
+//   return countriesData.map((country) => {
+//     data = { country: country.country };
+//     fields.forEach((field) => {
+//       data = { ...data, [field]: country[field] };
+//     });
+//     return data;
+//   });
+// }
+
+// console.log(getData(["capital", "currency"]));
+
+// // console.log(countries);
